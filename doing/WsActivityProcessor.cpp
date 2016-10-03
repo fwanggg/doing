c1e44098fb5b34633cd53a7b8bd6c0904bf1f6ef
fan wang <fwang@riverbed.com>
2016-10-03 00:36:54 -0400

    fixed websocket retry protocol

--- doing/WsActivityProcessor.cpp
+++ doing/WsActivityProcessor.cpp
@@ -2,20 +2,22 @@
 #include <Windows.h>
 #include <thread>
 #include <iostream>
-#include "easywsclient.hpp"
 #include "StringUtil.h"
-using easywsclient::WebSocket;
+#include "SimpleLogger.h"
+#include <sstream>
 WsActivityProcessor::WsActivityProcessor(const std::wstring & ws_endpoint, ConcurrentQueue<Activity>& job_queue)
-    :_ws_endpoint(ws_endpoint), ActivityProcessor(job_queue)
+    :_ws_endpoint(ws_endpoint), 
+    ActivityProcessor(job_queue), 
+    _ws_endpoint_str(_ws_endpoint.begin(), _ws_endpoint.end())
 {
     
 }
 void WsActivityProcessor::ProcessAcitivityQueue()
 {
     //conversion assumes the ws endpoint contains only ASCII characters
-    std::string ws_endpoint_str(_ws_endpoint.begin(),_ws_endpoint.end());
+    //std::string _ws_endpoint_str(_ws_endpoint.begin(),_ws_endpoint.end());
     //anything bad happens, from_url returns NULL
-    std::unique_ptr<WebSocket> ws(WebSocket::from_url(ws_endpoint_str));
+    ws.reset(WebSocket::from_url(_ws_endpoint_str));
     bool connOK = false;
     while (true)
     {
@@ -26,31 +28,45 @@ void WsActivityProcessor::ProcessAcitivityQueue()
             //doing real processing work
             //=================================
             //use easywsclient to talk to server via ws api
+            connOK = ws && ws->getReadyState() != WebSocket::CLOSED;
             if (connOK)
             {
                 Activity activity = m_job_queue.GetAndPop();
                 std::wstring wc_str = activity.GetFormattedReport();
-                std::string mb_str(wc_str.begin(),wc_str.end()); //TODO: handle i18n later
+                //std::string mb_str(wc_str.begin(),wc_str.end()); //TODO: handle i18n later
+                std::string mb_str = StringUtilBase::ConvertUnicodeToUTF8(wc_str);
                 ws->send(mb_str);   //according to easywsclient's doc, this only puts message into a buffer
                 ws->poll(1000);     //while this actually does the real send I/O
                                     //for some reason, stl queue pop just don't return you the element
+                std::wstringstream wss;
+                wss << L"Sent : " << wc_str;
+                SimpleLogger::GetInstance().WriteLogMessage(INFO_LVL, wss.str());
+            }
+            else
+            {
+                RetryAfter(10000);
+                connOK = ws && ws->getReadyState() != WebSocket::CLOSED;
             }
         }
         Sleep(500);
         //ws disconnect for some reason. Now retry.
         if (!connOK)
         {
-            if (ws)
-            {
-                //to avoid leak any resource, release ws now
-                //TODO: read its source code and determine if this is necessary
-                ws->close();
-            }
-            ws.reset(WebSocket::from_url(ws_endpoint_str));
-            //retry connection every 10 seconds
-            Sleep(10000);
+            RetryAfter(10000);
         }
     }
+}
+void WsActivityProcessor::RetryAfter(int ms)
+{
+    if (ws)
+    {
+        //to avoid leak any resource, release ws now
+        //TODO: read its source code and determine if this is necessary
+        ws->close();
+    }
+    //retry connection every 10 seconds
+    Sleep(ms);
+    ws.reset(WebSocket::from_url(_ws_endpoint_str));
 }
 
 WsActivityProcessor::~WsActivityProcessor()
--- doing/WsActivityProcessor.h
+++ doing/WsActivityProcessor.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <queue>
+#include <memory>
 #include "Activity.h"
 #include "ConcurrentQueue.h"
 #include "ActivityProcessor.h"
+#include "easywsclient.hpp"
+using easywsclient::WebSocket;
 class WsActivityProcessor : public ActivityProcessor
 {
 public:
@@ -12,4 +15,9 @@ public:
     virtual ~WsActivityProcessor();
 private:
     std::wstring _ws_endpoint;
+    std::string _ws_endpoint_str;
+    //don't initialize websocket in constructor, because i want this object to be created no matter what
+    //then handling failure in the ProcessAcitivityQueue method
+    std::shared_ptr<WebSocket> ws;
+    void RetryAfter(int ms);
 };
